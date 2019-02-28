0ac85fdd6ddd49e34edcdd00785be8c5a48425fd
Karen Shea <shea.karene@gmail.com>
2019-02-27 20:36:14 -0500

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Sluts</h1>
+{/* karen put some stuff here */}
+
+      </div>
+    );
+  }
+}
+
+export default App;
