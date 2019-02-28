31851255dd2bd7684473222a3b90d0311047546a
Karen Shea <shea.karene@gmail.com>
2019-02-28 14:16:48 -0500

    API key added
    Test to make sure I properly added config.js to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import apiKey from './src/config.js';
 
 class App extends Component {
+
+
+
+
+
+  
   render() {
     return (
       <div className="App">
         <h1>Sluts</h1>
-{/* karen put some stuff here */}
+
 
       </div>
     );
